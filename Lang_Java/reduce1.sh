2837228a25f3ba4454b7082f8f1da8bd315c01e3
Jens Krinke <krinke@acm.org>
2021-07-04 13:44:29 +0000

    Decided to go for a 30/30 filter.

--- Lang_Java/reduce1.sh
+++ Lang_Java/reduce1.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 ../_reduce.sh gcj 30 '^ *[ 12][0-9] '
+../_reduce.sh gcj 30 '^ *30 '
 
 # Done.
--- Lang_Java/reduce2.sh
+++ Lang_Java/reduce2.sh
@@ -2,9 +2,9 @@
 
 # 38 is currently the maximal value that works.
 
-../_reduce.sh gcj50java 38 '^ *[ 12][0-9] '
-../_reduce.sh gcj50java 38 '^ *3[012345678] '
-../_reduce.sh gcjD50java 38 '^ *[ 12][0-9] '
-../_reduce.sh gcjD50java 38 '^ *3[012345678] '
+../_reduce.sh gcj50java 30 '^ *[ 12][0-9] '
+../_reduce.sh gcj50java 30 '^ *30 '
+../_reduce.sh gcjD50java 30 '^ *[ 12][0-9] '
+../_reduce.sh gcjD50java 30 '^ *30 '
 
 # Done.
