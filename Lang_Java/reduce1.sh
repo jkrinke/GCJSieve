2744c7ad8ae36450521cb463334fbb83d688aca7
Jens Krinke <krinke@acm.org>
2021-07-04 19:52:52 +0100

    Reverted to less than (less or equal was a mistake).

--- Lang_Java/reduce1.sh
+++ Lang_Java/reduce1.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 ../_reduce.sh gcj 30 '^ *[ 12][0-9] '
-../_reduce.sh gcj 30 '^ *30 '
 
 # Done.
--- Lang_Java/reduce2.sh
+++ Lang_Java/reduce2.sh
@@ -3,8 +3,6 @@
 # 38 is currently the maximal value that works.
 
 ../_reduce.sh subset 30 '^ *[ 12][0-9] '
-../_reduce.sh subset 30 '^ *30 '
 ../_reduce.sh subset-d 30 '^ *[ 12][0-9] '
-../_reduce.sh subset-d 30 '^ *30 '
 
 # Done.
